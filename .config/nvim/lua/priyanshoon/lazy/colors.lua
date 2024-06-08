629449edd9daeafea6f4cded7ce4ad7eb9c49172
priyanshoon <guptapriyanshu973@gmail.com>
2024-06-08 10:06:56 +0530

    dotfiles changes, added alacritty config

--- .config/nvim/lua/priyanshoon/lazy/colors.lua
+++ .config/nvim/lua/priyanshoon/lazy/colors.lua
@@ -8,63 +8,28 @@ end
 
 return {
     {
-        "ramojus/mellifluous.nvim",
-        name = "mellifluous",
-        config = function()
-            require 'mellifluous'.setup({
-                dim_inactive = false,
-                color_set = 'kanagawa_dragon',
-                styles = { -- see :h attr-list for options. set {} for NONE, { option = true } for option
-                    comments = { italic = true },
-                    conditionals = {},
-                    folds = {},
-                    loops = {},
-                    functions = {},
-                    keywords = {},
-                    strings = {},
-                    variables = {},
-                    numbers = {},
-                    booleans = {},
-                    properties = {},
-                    types = {},
-                    operators = {},
-                    markup = {
-                        headings = { bold = true },
-                    },
-                },
-                transparent_background = {
-                    enabled = false,
-                    floating_windows = true,
-                    telescope = true,
-                    file_tree = true,
-                    cursor_line = false,
-                    status_line = false,
-                },
-                flat_background = {
-                    line_numbers = false,
-                    floating_windows = false,
-                    file_tree = false,
-                    cursor_line_number = false,
-                },
-                plugins = {
-                    cmp = true,
-                    gitsigns = true,
-                    indent_blankline = true,
-                    nvim_tree = {
-                        enabled = true,
-                        show_root = false,
-                    },
-                    neo_tree = {
-                        enabled = true,
-                    },
-                    telescope = {
-                        enabled = true,
-                        nvchad_like = false,
-                    },
-                    startify = true,
+        "priyanshoon/rosepine",
+        name = "rosepine",
+        config = function ()
+            local rosepine = require("rose-pine")
+            rosepine.setup({
+                styles = {
+                    bold = true,
+                    italic = true,
+                    transparency = false,
                 },
             })
-            ColorMyPencils("mellifluous")
+            -- ColorMyPencils("rose-pine-main")
+        end
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        name = "gruvbox",
+        config = function()
+            local gruvbox = require("gruvbox")
+            gruvbox.setup({
+            })
+            ColorMyPencils("gruvbox")
         end
     }
 }
